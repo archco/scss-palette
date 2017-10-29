80fc303a1ba99af65a9721cdb0dcd2d12369ed70
archco <archcoster@gmail.com>
2017-10-29 13:12:35 +0900

    Modify dev environment files.

--- task/banner.js
+++ task/banner.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const path = require('path');
-const pkg = require('./package.json');
+const pkg = require('../package.json');
 
 // jscs:disable
 const banner = `/*! scss-palette v${pkg.version} | #{length($palette)} colors | ${pkg.homepage} | ${pkg.license} License */`;
 // jscs:enable
 
-const file = path.join(__dirname, './src/_palette.scss');
+const file = path.join(__dirname, '../src/_palette.scss');
 
 fs.readFile(file, 'utf8', (err, data) => {
   if (err) throw err;
---
+++ task/convert-to-json.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const replace = require('replace-in-file');
+
+const target = path.resolve('test/colors.css');
+const dest = path.resolve('test/colors.json');
+
+// convert colors.css to colors.json
+// 1. strip comments syntax.
+replace({
+  files: target,
+  from: [/(\/\*\!).*/gm, /(\/\*)|(\*\/)/g],
+  to: '',
+})
+.then(changedFiles => {
+  console.log('Modified files:', changedFiles.join(', '));
+
+  // 2. rename to colors.json
+  fs.rename(target, dest, (err) => {
+    if (err) throw err;
+    console.log('Renamed complete');
+  });
+})
+.catch(error => {
+  console.error('Error occurred:', error);
+});;
